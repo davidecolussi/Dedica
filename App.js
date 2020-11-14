b9a6c0dcc2d38500dbae6fa524661cc2e425284f
davide <davide.colussi.99@gmail.com>
2020-11-14 19:41:35 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+import SubjectTime from './components/SubjectTime';
+
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
 
 export default function App() {
   return (
+    <View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <SubjectTime style={styles.subjectTime} subject_name="Fondamenti di controlli automatici" cfu="9" dedicaTime="35 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Analisi I" cfu="12" dedicaTime="50 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+        <SubjectTime style={styles.subjectTime} subject_name="Complementi di Fisica II" cfu="6" dedicaTime="30 ore"/>
+
+
+      </ScrollView>
+
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
     backgroundColor: '#fff',
+  },
+  subjectTime: {
+    flex: 1,
+    backgroundColor: '#f0f',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/SubjectTime.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
+
+
+
+class SubjectTime extends React.Component {
+
+    render() {
+        return (
+          <View style={styles.container}>
+            <Text style={styles.subjectText}>{this.props.subject_name}</Text>
+            <Text style={styles.cfuText}>Cfu dichiarati: {this.props.cfu}</Text>
+            <Text style={styles.dedicaText}>Tempo di studio effettivo da dedicare: {this.props.dedicaTime}</Text>
+          </View>
+        );
+    };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    'marginBottom': 10
+  },
+  subjectText: {
+    'backgroundColor': "#fff00f",
+  },
+  cfuText: {
+    'backgroundColor': "lightblue"
+  },
+  dedicaText: {
+    'backgroundColor': "lightgreen"
+  }
+});
+export default SubjectTime;
